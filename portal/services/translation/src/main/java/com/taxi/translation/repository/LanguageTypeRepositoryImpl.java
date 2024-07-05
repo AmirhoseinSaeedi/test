ee7189800634349e002b1387ee25c2b0352627ad
aminhzdev <amin.hasan.zarei@gmail.com>
2024-07-05 14:47:51 +0330

    fix translation

--- portal/services/translation/src/main/java/com/taxi/translation/repository/LanguageTypeRepositoryImpl.java
+++
@@ -1,8 +0,0 @@
-package com.taxi.translation.repository;
-
-import com.taxi.framework.translation.Repository.AbstractLanguageTypeRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LanguageTypeRepositoryImpl extends AbstractLanguageTypeRepository {
-}
